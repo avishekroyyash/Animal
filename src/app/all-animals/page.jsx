fbd7334d1ddc57c0385beeda23688d3c7bbe130f
avishekroyyash <avishekroyyash@gmail.com>
2026-05-21 20:40:44 +0600

    Add navber with design

---
+++ src/app/all-animals/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllAnimalPage = () => {
+    return (
+        <div>
+           this is all animal page 
+        </div>
+    );
+};
+
+export default AllAnimalPage;
---
+++ src/component/Navber.jsx
@@ -0,0 +1,28 @@
+'use client'
+import Link from 'next/link';
+import React from 'react';
+import logo from '@/Asset/cow-logo.jpg'
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+const Navber = () => {
+    const pathname = usePathname();
+    console.log(pathname,'this is path name')
+    return (
+        <div className='flex justify-between items-center w-400 mx-auto border-2 p-2'>
+            <div>
+             <Image className='bg-green-600 rounded-full p-1' src={logo} alt='cow' width={100} height={100}></Image>
+            </div>
+             <div className='flex justify-center items-center gap-5 '>
+             <Link className={`font-bold text-xl ${pathname=='/' && 'bg-green-800 text-white p-2 rounded-xl'}`} href={'/'}>Home</Link> 
+             <Link className={`font-bold text-xl ${pathname=='/all-animals' && 'bg-green-800 text-white p-2 rounded-xl'}`}  href={'/all-animals'}>All Animal</Link> 
+             <Link  className={`font-bold text-xl ${pathname=='/profile' && 'bg-green-800 text-white p-2 rounded-xl'}`}  href={'/profile'}>Profile</Link>  
+             </div>
+             <div className='flex gap-4'>
+               <Link href={'/login'}>  <button className='btn bg-green-800 rounded-xl p-3 text-white hover:bg-green-700 text-xl font-bold'>Login</button></Link>
+                <Link href={'/resister'}> <button className='btn bg-green-800 rounded-xl p-3 text-white hover:bg-green-700 text-xl font-bold'>Resister</button></Link>
+             </div>
+        </div>
+    );
+};
+
+export default Navber;
